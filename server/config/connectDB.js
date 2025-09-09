fc72dcd31c1a4b22189ed021a8dbacdb03ed4250
Dheeraj Pratap Singh <singh.dheeraj@outlook.in>
2025-09-09 09:22:32 +0530

    Initial Boilerplate Commit

---
+++ server/config/connectDB.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
+
+if(!process.env.MONGODB_URI){
+    throw new Error(
+        "Please provide MONGODB_URI in the .env file"
+    )
+}
+
+async function connectDB(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("connect DB")
+    } catch (error) {
+        console.log("Mongodb connect error",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+import morgan from 'morgan'
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
+import userRouter from './route/user.route.js'
+
+
+const app = express()
+app.use(cors({
+    credentials : true,
+    origin : process.env.FRONTEND_URL
+}))
+console.log("frontendURI:", process.env.FRONTEND_URL);
+
+app.use(express.json())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy : false
+}))
+
+const PORT = process.env.PORT || 8080
+
+
+app.get("/",(request,response)=>{
+    ///server to client
+    response.json({
+        message : "Server is running " + PORT
+    })
+})
+
+app.use('/api/user',userRouter)
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running",PORT)
+    })
+})
+
+
